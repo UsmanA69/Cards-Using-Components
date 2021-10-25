4663c851d0f8c51aafeb3da60125407dd5ad7fda
UsmanA69 <64627750+UsmanA69@users.noreply.github.com>
2021-10-25 22:01:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Card from "./Card";
+import CardGallery from "./Cardgallery";
 
 function App() {
   return (
     <>
-      <Card />
+
+      <CardGallery />
     </>
   );
 }
